a38fa3b75985d657358c320ac0fa04a6aafab733
obaidullah1209 <mobaidullah554@gmail.com>
2024-03-10 05:23:25 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var userName = prompt("Enter Your BFF Name Here");
+if(userName === "obaid"){
+    alert("YES HE IS A GOOD BOY AND A GREEN FLAG")
+}
+else if (userName === "Obaid"){
+    alert("YES HE IS A GOOD BOY AND A GREEN FLAG")
+}
+else if (userName === "zara"){
+    alert("SHE IS GOOD BUT NOT LIKE OBAID")
+}
+else if (userName === "Zara"){
+    alert("SHE IS GOOD BUT NOT LIKE OBAID")
+}
+else {
+    alert(" YOU ASS HOLE DESERVE A RED FLAG LIKE ISMAIL")
+}
